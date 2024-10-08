3003dca6eca1ec2f54d517d1f8072e35b806451d
mchoubisa97 <mchoubisa0@gmail.com>
2024-10-08 20:30:18 +0530

    Revert "json format output"
    This reverts commit 8a430037c9be62707461c281da20a0f151df6b1e.

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,4 @@
-import json
 import random
 
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(random.randint(0, 1000))
-    }
+def handler(event, context):
+    return random.randint(0, 1000)
