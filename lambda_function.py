e188509b284f98ca62fd6f50fb47cda9367e5d29
mchoubisa97 <mchoubisa0@gmail.com>
2024-10-08 20:13:52 +0530

    test

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,4 @@ import json
 import random
 
 def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(random.randint(0, 1000))
-    }
+    return random.randint(0, 1000)
